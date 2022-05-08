140fc15bca27784751f1412872c4f4345e1d2c2b
bpsha <bps0075>
2022-05-07 23:49:04 -0500

    Commenting test code

--- outputScreen.py
+++ outputScreen.py
@@ -1,6 +1,6 @@
-#Brandon Sharp
-#This file plots and outputs the x and y data onto the screen
-#uses data from other files
+# Brandon Sharp
+# This file plots and outputs the x and y data onto the screen
+# uses data from other files
 import networkx as nx
 import math
 import matplotlib.pyplot as plt
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 class outputScreen:
 
     def xyplot(x, y):
-        plt.plot(x, y) #plots the points for each x and y value
+        plt.plot(x, y) # plots the points for each x and y value
     def output(x, y):
-        plt.show() #outputs the graph
+        plt.show() # outputs the graph
 
 
-x = [1, 2, 3, 4, 5] #testing
-y = x #testing
-outputScreen.xyplot(x, y) #testing
-outputScreen.output(x, y) #testing
+# x = [1, 2, 3, 4, 5] # testing
+# y = x # testing
+# outputScreen.xyplot(x, y) # testing
+# outputScreen.output(x, y) # testing
