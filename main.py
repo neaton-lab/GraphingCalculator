df113794251ac6fe3d51faeb67a30bbad5d14509
Andrew Herubin <andrewherubin@outlook.com>
2022-05-07 23:22:28 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import input
 import renderFunction
 import outputScreen
 
+def main():
+    read = input.Input()
+    function = renderFunction.RenderFunction(read)
+    output = function.render(0, 10, 5)
+    outputScreen.xyplot(output[0], output[1])
+    outputScreen.output(0, 0)
+
+# execution
+if __name__ == "__main__":
+    main()
